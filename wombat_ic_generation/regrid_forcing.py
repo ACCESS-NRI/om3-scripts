0e1072d22370cecb32b6cd8103d19cf2d47fe5a3
dougiesquire <dougiesquire@gmail.com>
2025-06-17 09:10:07 +1000

    wombat_ic_generation: add script for regridding forcing

---
+++ wombat_ic_generation/regrid_forcing.py
@@ -0,0 +1,193 @@
+# Copyright 2023 ACCESS-NRI and contributors. See the top-level COPYRIGHT file for details.
+# SPDX-License-Identifier: Apache-2.0
+
+# =========================================================================================
+# Interpolate a provided forcing file to a grid specified by a provided MOM supergrid file for
+# use in the MOM data_table.
+#
+# To run:
+#   python regrid_forcing.py --forcing-filename=<path-to-forcing-file>
+#       --hgrid-filename=<path-to-supergrid-file> --output-file=<path-to-output-file>
+# these extra arguments are available if required:
+#     --lon-name=<lon-name> --lat-name=<lat-name>
+#
+# For more information, run `python regrid_forcing.py -h`
+#
+# The run command and full github url of the current version of this script is added to the
+# metadata of the generated file. This is to uniquely identify the script and inputs used to
+# generate the file. To produce files for sharing, ensure you are using a version of this script
+# which is committed and pushed to github. For files intended for released configurations, use the
+# latest version checked in to the main branch of the github repository.
+#
+# Contact:
+#   Dougie Squire <dougal.squire@anu.edu.au>
+#
+# Dependencies:
+#   argparse, xarray, cf_xarray, xesmf
+# =========================================================================================
+
+import os
+import sys
+from pathlib import Path
+
+import xarray as xr
+import xesmf as xe
+
+path_root = Path(__file__).parents[1]
+sys.path.append(str(path_root))
+
+from scripts_common import get_provenance_metadata, md5sum
+
+
+def _guess_longitude_name(ds):
+    coords = ds.cf.coordinates
+    if "longitude" in coords:
+        if len(coords["longitude"]) == 1:
+            return coords["longitude"][0]
+        else:
+            raise KeyError("Multiple longitude variables exist. Please pass --lon-name")
+    elif "lon" in ds:
+        return "lon"
+    else:
+        raise KeyError(
+            "Cannot automatically determine the name of the longitude variable. Please pass --lon-name"
+        )
+
+
+def _guess_latitude_name(ds):
+    coords = ds.cf.coordinates
+    if "latitude" in coords:
+        if len(coords["latitude"]) == 1:
+            return coords["latitude"][0]
+        else:
+            raise KeyError("Multiple latitude variables exist. Please pass --lat-name")
+    elif "lat" in ds:
+        return "lat"
+    else:
+        raise KeyError(
+            "Cannot automatically determine the name of the latitude variable. Please pass --lat-name"
+        )
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description=(
+            "Interpolate a provided forcing file to a grid specified by a provided MOM supergrid"
+            "file for use in the MOM data_table."
+        )
+    )
+
+    parser.add_argument(
+        "--forcing-filename",
+        required=True,
+        help="The path to the forcing file to interpolate.",
+    )
+
+    parser.add_argument(
+        "--hgrid-filename",
+        required=True,
+        help="The path to the MOM supergrid file to use as a grid for interpolation.",
+    )
+
+    parser.add_argument(
+        "--output-file",
+        required=True,
+        help="The path to the file to be outputted.",
+    )
+
+    parser.add_argument(
+        "--lon-name",
+        type=str,
+        default=None,
+        help="The name of the longitude variable in the input grid. If not passed, an attempt will be made to guess the name.",
+    )
+
+    parser.add_argument(
+        "--lat-name",
+        type=str,
+        default=None,
+        help="The name of the latitude variable in the input grid. If not passed, an attempt will be made to guess the name.",
+    )
+
+    args = parser.parse_args()
+    forcing_filename = os.path.abspath(args.forcing_filename)
+    hgrid_filename = os.path.abspath(args.hgrid_filename)
+    output_file = os.path.abspath(args.output_file)
+    lon_name = args.lon_name
+    lat_name = args.lat_name
+
+    this_file = os.path.normpath(__file__)
+
+    # Add some info about how the file was generated
+    runcmd = (
+        f"python3 {os.path.basename(this_file)} --forcing-filename={forcing_filename} "
+        f"--hgrid-filename={hgrid_filename} --output-file={output_file}"
+    )
+    if lon_name:
+        runcmd += f" --lon-name={lon_name}"
+    if lat_name:
+        runcmd += f" --lat-name={lat_name}"
+
+    file_hashes = [
+        f"{forcing_filename} (md5 hash: {md5sum(forcing_filename)})",
+        f"{hgrid_filename} (md5 hash: {md5sum(hgrid_filename)})",
+    ]
+    global_attrs = {
+        "history": get_provenance_metadata(this_file, runcmd),
+        "inputFile": ", ".join(file_hashes),
+    }
+
+    # Load the input data
+    forcing_src = xr.open_dataset(forcing_filename).compute()
+    hgrid = xr.open_dataset(hgrid_filename).compute()
+
+    # Standardise lon/lat coordinate names
+    if not lon_name:
+        lon_name = _guess_longitude_name(forcing_src)
+
+    if not lat_name:
+        lat_name = _guess_latitude_name(forcing_src)
+
+    forcing_src = forcing_src.rename({lon_name: "lon", lat_name: "lat"})
+
+    # Get source and destination grid
+    grid_src = forcing_src[["lon", "lat"]]
+
+    lon_dest = hgrid["x"][1:-1:2, 1:-1:2].to_dataset(name="lon")
+    lat_dest = hgrid["y"][1:-1:2, 1:-1:2].to_dataset(name="lat")
+    grid_dest = xr.merge((lon_dest, lat_dest))
+
+    # Regrid using bilinear interpolation
+    regridder = xe.Regridder(grid_src, grid_dest, method="bilinear", periodic=True)
+    forcing_regrid = regridder(forcing_src)
+
+    # Add coodinates and metadata required by data_table
+    forcing_regrid = forcing_regrid.assign_coords(
+        lon=lon_dest["lon"], lat=lat_dest["lat"]
+    )
+    forcing_regrid = forcing_regrid.assign_coords(
+        {
+            "nyp": ("nyp", range(forcing_regrid.sizes["nyp"])),
+            "nxp": ("nxp", range(forcing_regrid.sizes["nxp"])),
+        }
+    )
+    forcing_regrid["time"].attrs = dict(axis="T", modulo="y")
+    forcing_regrid["nyp"].attrs = dict(axis="Y")
+    forcing_regrid["nxp"].attrs = dict(axis="X")
+    forcing_regrid["lat"].attrs = dict(
+        long_name="Latitude of T-cell center", units="degree_north"
+    )
+    forcing_regrid["lon"].attrs = dict(
+        long_name="Longitude of T-cell center",
+        units="degrees_east",
+    )
+    forcing_regrid.attrs = forcing_regrid.attrs | global_attrs
+
+    # Save output
+    forcing_regrid.to_netcdf(output_file, unlimited_dims="time")
+
+
+if __name__ == "__main__":
+    import argparse
+
+    main()
