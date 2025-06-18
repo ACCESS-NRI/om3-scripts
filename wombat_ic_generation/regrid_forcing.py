aa180366b62e3ca03de739048f28040d1699d84e
dougiesquire <dougiesquire@gmail.com>
2025-06-18 20:46:24 +1000

    Add option to pass land-sea mask

--- wombat_ic_generation/regrid_forcing.py
+++ wombat_ic_generation/regrid_forcing.py
@@ -7,9 +7,9 @@
 #
 # To run:
 #   python regrid_forcing.py --forcing-filename=<path-to-forcing-file>
-#       --hgrid-filename=<path-to-supergrid-file> --output-filename=<path-to-output-file>
+#     --hgrid-filename=<path-to-supergrid-file> --output-filename=<path-to-output-file>
 # these extra arguments are available if required:
-#     --lon-name=<lon-name> --lat-name=<lat-name>
+#     --mask-filename=<path-to-mask-file>--lon-name=<lon-name> --lat-name=<lat-name>
 #
 # For more information, run `python regrid_forcing.py -h`
 #
@@ -79,20 +79,30 @@ def main():
 
     parser.add_argument(
         "--forcing-filename",
+        type=str,
         required=True,
         help="The path to the forcing file to interpolate.",
     )
 
     parser.add_argument(
         "--hgrid-filename",
+        type=str,
         required=True,
         help="The path to the MOM supergrid file to use as a grid for interpolation.",
     )
 
     parser.add_argument(
         "--output-filename",
+        type=str,
         required=True,
         help="The path to the file to be outputted.",
+    )
+
+    parser.add_argument(
+        "--mask-filename",
+        type=str,
+        default=None,
+        help="The path to a land-sea (0-1) mask for the forcing file. Cells that fall on land in the destination grid will take their values from the nearest source grid cell.",
     )
 
     parser.add_argument(
@@ -113,8 +123,12 @@ def main():
     forcing_filename = os.path.abspath(args.forcing_filename)
     hgrid_filename = os.path.abspath(args.hgrid_filename)
     output_filename = os.path.abspath(args.output_filename)
+    mask_filename = args.mask_filename
     lon_name = args.lon_name
     lat_name = args.lat_name
+
+    if mask_filename:
+        mask_filename = os.path.abspath(mask_filename)
 
     this_file = os.path.normpath(__file__)
 
@@ -123,6 +137,8 @@ def main():
         f"python3 {os.path.basename(this_file)} --forcing-filename={forcing_filename} "
         f"--hgrid-filename={hgrid_filename} --output-filename={output_filename}"
     )
+    if mask_filename:
+        runcmd += f" --mask-filename={mask_filename}"
     if lon_name:
         runcmd += f" --lon-name={lon_name}"
     if lat_name:
@@ -132,6 +148,8 @@ def main():
         f"{forcing_filename} (md5 hash: {md5sum(forcing_filename)})",
         f"{hgrid_filename} (md5 hash: {md5sum(hgrid_filename)})",
     ]
+    if mask_filename:
+        file_hashes.append(f"{mask_filename} (md5 hash: {md5sum(mask_filename)})")
     global_attrs = {
         "history": get_provenance_metadata(this_file, runcmd),
         "inputFile": ", ".join(file_hashes),
@@ -140,6 +158,8 @@ def main():
     # Load the input data
     forcing_src = xr.open_dataset(forcing_filename).compute()
     hgrid = xr.open_dataset(hgrid_filename).compute()
+    if mask_filename:
+        forcing_mask = xr.open_dataset(mask_filename).compute()
 
     # Standardise lon/lat coordinate names
     if not lon_name:
@@ -152,29 +172,39 @@ def main():
 
     # Get source and destination grid
     grid_src = forcing_src[["lon", "lat"]]
+    if mask_filename:
+        # Add the mask to the source grid so that land values are extrapolated
+        grid_src = grid_src.assign(forcing_mask)
 
     # Destination grid is tracer cell centres
     lon_dest = hgrid["x"][1:-1:2, 1:-1:2].to_dataset(name="lon")
     lat_dest = hgrid["y"][1:-1:2, 1:-1:2].to_dataset(name="lat")
     grid_dest = xr.merge((lon_dest, lat_dest))
 
-    # Regrid using bilinear interpolation
-    regridder = xe.Regridder(grid_src, grid_dest, method="bilinear", periodic=True)
+    # Regrid using bilinear interpolation with nearest neighbour extrapolation
+    regridder = xe.Regridder(
+        grid_src,
+        grid_dest,
+        method="bilinear",
+        extrap_method="nearest_s2d",
+        periodic=True,
+    )
     forcing_regrid = regridder(forcing_src)
 
     # Add coodinates and metadata required by data_table
     forcing_regrid = forcing_regrid.assign_coords(
         lon=lon_dest["lon"], lat=lat_dest["lat"]
     )
+    forcing_regrid = forcing_regrid.rename({"nyp": "ny", "nxp": "nx"})
     forcing_regrid = forcing_regrid.assign_coords(
         {
-            "nyp": ("nyp", range(forcing_regrid.sizes["nyp"])),
-            "nxp": ("nxp", range(forcing_regrid.sizes["nxp"])),
+            "ny": ("ny", range(forcing_regrid.sizes["ny"])),
+            "nx": ("nx", range(forcing_regrid.sizes["nx"])),
         }
     )
     forcing_regrid["time"].attrs = dict(axis="T", standard_name="time", modulo="y")
-    forcing_regrid["nyp"].attrs = dict(axis="Y")
-    forcing_regrid["nxp"].attrs = dict(axis="X")
+    forcing_regrid["ny"].attrs = dict(axis="Y")
+    forcing_regrid["nx"].attrs = dict(axis="X")
     forcing_regrid["lat"].attrs = dict(
         long_name="Latitude of T-cell center",
         standard_name="latitude",
@@ -188,7 +218,12 @@ def main():
     forcing_regrid.attrs = forcing_regrid.attrs | global_attrs
 
     # Save output
-    forcing_regrid.to_netcdf(output_filename, unlimited_dims="time")
+    comp = dict(zlib=True, complevel=4)
+    encoding = {var: comp for var in forcing_regrid.data_vars}
+    unlimited_dims = "time" if "time" in forcing_regrid.dims else None
+    forcing_regrid.to_netcdf(
+        output_filename, unlimited_dims=unlimited_dims, encoding=encoding
+    )
 
 
 if __name__ == "__main__":
