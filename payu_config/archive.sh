35ecdba61f84e5f7d9f04277f1e95737a2f7fab3
anton-seaice <anton.steketee@anu.edu.au>
2025-07-14 16:06:31 +1000

    move intake datastore generation into postscript

--- payu_config/archive.sh
+++ payu_config/archive.sh
@@ -1,4 +1,2 @@
 source $(dirname "$0")/archive_scripts/concat_ice_daily.sh
 source $(dirname "$0")/archive_scripts/standardise_mom6_filenames.sh
-
-qsub -lstorage=gdata/xp65+${PBS_NCI_STORAGE} -v SCRIPTS_DIR=$(dirname "$0"),PROJECT $(dirname "$0")/archive_scripts/build_intake_ds.sh
---
+++ payu_config/postscript/build_intake_ds.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+#PBS -l ncpus=104
+#PBS -l mem=500GB
+#PBE -l jobfs=100GB
+#PBS -q expresssr
+#PBS -l walltime=00:30:00
+#PBS -l wd
+#PBS -v PYTHONNOUSERSITE=True
+
+module purge
+module use /g/data/xp65/public/modules 
+module load conda/analysis3-25.05
+
+python3 $SCRIPTS_DIR/postscript/build_intake_ds.py
