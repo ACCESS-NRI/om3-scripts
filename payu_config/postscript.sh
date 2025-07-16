497943c82fd4e989c5493d39894ee094efd40572
Anton Steketee <79179784+anton-seaice@users.noreply.github.com>
2025-07-16 11:44:38 +1000

    move intake datastore generation into postscript (#71)

---
+++ payu_config/postscript.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+# Copyright 2025 ACCESS-NRI and contributors. See the top-level COPYRIGHT file for details.
+# SPDX-License-Identifier: Apache-2.0
+#PBS -l ncpus=104
+#PBS -l mem=500GB
+#PBE -l jobfs=100GB
+#PBS -q expresssr
+#PBS -l walltime=00:30:00
+#PBS -l wd
+#PBS -v PYTHONNOUSERSITE=True
+#PBS -j oe
+
+module purge
+module use /g/data/xp65/public/modules 
+module load conda/analysis3-25.05
+
+python3 $SCRIPTS_DIR/payu_config/postscript/build_intake_ds.py
