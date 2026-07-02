4057f06725d8ca45f77c74effc685a1e51f8819e
anton-seaice <anton-seaice@users.noreply.github.com>
2026-07-02 15:41:24 +1000

    First pass a test to show conservation

--- test/test_mesh/test_rof_weights.py
+++ test/test_mesh/test_rof_weights.py
@@ -3,24 +3,26 @@
 
 import pytest
 from unittest.mock import patch
-from mesh_generation.generate_mesh import mom6_mask_detection, MomSuperGrid
 from pathlib import Path
 from subprocess import run
+
 import xarray as xr
 import numpy as np
 import esmpy
+from copy import copy
+
+from mesh_generation.generate_mesh import mom6_mask_detection, MomSuperGrid
+from mesh_generation.generate_rof_weights import drof_remapping_weights
 
 # create test grids at 4 degrees and 0.1 degrees
 # 4 degress is the lowest tested in ocean_model_grid_generator
 # going higher resolution than 0.1 has too much computational cost
 _test_resolutions = [4]  # , 0.1]
 
-
 # so that our fixtures are only created once in this pytest module, we need this special version of 'tmp_path'
 @pytest.fixture(scope="module")
-def tmp_path(tmp_path_factory: pytest.TempdirFactory) -> Path:
+def grid_path(tmp_path_factory: pytest.TempdirFactory) -> Path:
     return tmp_path_factory.mktemp("temp")
-
 
 # ----------------
 # test data:
@@ -54,18 +56,55 @@ class MomGridFixture:
         ) > 5e9
         self.mask_ds.to_netcdf(self.mask_path)
 
+        self.ny = int(len(self.ds.ny)/2)
+        self.nx = int(len(self.ds.nx)/2)
 
-# pytest doesn't support class fixtures, so we need this constructor
+
+# pytest doesn't support class fixtures, so we need these constructor
 @pytest.fixture(scope="module", params=_test_resolutions)
-def mom_grid(request, tmp_path):
-    return MomGridFixture(request.param, tmp_path)
+def mom_grid(request, grid_path):
+    return MomGridFixture(request.param, grid_path)
 
+def mesh_creator(filename):
+    """
+    Create esmpy flield object from a mesh file
+    """
+    mesh = esmpy.Mesh(
+        filename=filename,
+        filetype=esmpy.FileFormat.ESMFMESH,
+    )
+
+    fld = esmpy.Field(mesh, meshloc=esmpy.MeshLoc.ELEMENT)
+
+    fld.get_area()
+    area = copy(fld.data)
+
+    return {'fld':fld,'area': area}
 
 @pytest.fixture()
-def esmf_mask_mesh(mom_grid):
+def mesh_in(mom_grid, tmp_path):
     """
-    Patch the mom6_mask_detection function, and just return the mask from the mom_grid fixture
+    For the input mesh, use an unmasked mesh
     """
+    mesh_filename_in = str(tmp_path) + "/mesh_in.nc"
+
+    test_mesh = MomSuperGrid(mom_grid.path, topog_filename=None)
+    test_mesh.create_mesh()
+    test_mesh.write(mesh_filename_in)
+
+    result = mesh_creator(mesh_filename_in)
+    result['mom_super_grid'] = test_mesh
+
+    return result
+
+@pytest.fixture()
+def mesh_out(mom_grid, tmp_path):
+    """
+    Patch the mom6_mask_detection function, and use the mask from the mom_grid fixture,
+    rather than need a topog file
+    """
+    mesh_filename_out = str(tmp_path) + "/mesh_out.nc"
+
     with patch(
         "mesh_generation.generate_mesh.mom6_mask_detection",
         return_value=mom_grid.mask_ds.mask.values,
@@ -73,74 +112,66 @@ def esmf_mask_mesh(mom_grid):
         test_mesh = MomSuperGrid(mom_grid.path, topog_filename=mom_grid.mask_path)
 
     test_mesh.create_mesh()
+    test_mesh.write(mesh_filename_out)
 
-    test_mesh.write(str(tmp_path) + "/esmf_mask_mesh.nc")
+    result = mesh_creator(mesh_filename_out)
+    result['mom_super_grid'] = test_mesh
 
-    return test_mesh
-
+    return result
 
 @pytest.fixture
-def weights_file(esmf_mask_mesh, mom_grid):
+def weights_file(mesh_out, mom_grid, tmp_path):
+
     drof_remapping_weights(
-        str(tmp_path) + "/esmf_mask_mesh.nc",
+        str(tmp_path) + "/mesh_out.nc",
         str(tmp_path) + "/drof_remap_weights.nc",
-        len(mom_grid.ds.n1) / 2,
-        len(mom_grid.ds.ny) / 2,
+        mom_grid.nx,
+        mom_grid.ny,
     )
 
-    # check str(tmp_path) + "/drof_remap_weights.nc" exists ?
+    if not Path(str(tmp_path) + "/drof_remap_weights.nc").exists():
+        raise RuntimeError("drof remap weights not created")
 
     return str(tmp_path) + "/drof_remap_weights.nc"
 
+# ----------------
+# the actual tests:
 
-def test_generate_mask(esmf_mask_mesh, mom_grid):
+def test_generate_mask(mesh_out, mom_grid):
     """
-    This test just convinces us we can patch the mom6_mask_detection needing a
-    topography file, and instead provide an arbitrary mask when making a mesh
+    This test just convinces us the patch mom6_mask_detection in mesh_out works
     """
 
-    assert np.all(esmf_mask_mesh.mask == mom_grid.mask_ds.mask.values.flatten())
+    assert np.all(mesh_out['mom_super_grid'].mask == mom_grid.mask_ds.mask.values.flatten())
 
     assert (
-        len(esmf_mask_mesh.mesh.elementCount.values)
-        == len(mom_grid.ds.ny) / 2 * len(mom_grid.ds.nx) / 2
+        len(mesh_out['mom_super_grid'].mesh.elementCount.values)
+        == (mom_grid.ny * mom_grid.nx)
     )
 
+@pytest.mark.parametrize("data", ['All', 'None', 'Ocean', 'Land'])
+def test_regrid_conservation(data, mesh_in, mesh_out, weights_file, tmp_path):
+    """
+    For some provided meshes, and weights file, confirm that the weights are conservative
+    """
 
-# def test_generate_rof_weights(esmf_mask_mesh):
+    fld_in = mesh_in['fld']
+    area_in = mesh_in['area']
+    fld_out = mesh_out['fld']
+    area_out = mesh_out['area']
 
-
-@pytest.mark.parametrize("data", [1, 0])
-def test_regrid_conservation(data, esmf_mask_mesh, weights_file):
-
-    mesh_filename_in = str(tmp_path) + "/esmf_mask_mesh.nc"
-    mesh_filename_out = str(tmp_path) + "/esmf_mask_mesh.nc"
-
-    model_mesh_in = esmpy.Mesh(
-        filename=mesh_filename_in,
-        filetype=esmpy.FileFormat.ESMFMESH,
-    )
-
-    fld_in = esmpy.Field(model_mesh_in, meshloc=esmpy.MeshLoc.ELEMENT)
-
-    fld_in.get_area()
-    area_in = copy(fld_in.data)
-
-    model_mesh_out = esmpy.Mesh(
-        filename=mesh_filename_out,
-        filetype=esmpy.FileFormat.ESMFMESH,
-    )
-
-    fld_out = esmpy.Field(model_mesh_out, meshloc=esmpy.MeshLoc.ELEMENT)
-
-    fld_out.get_area()
-    area_out = copy(fld_out.data)
-
-    fld_in.data[:] = data
+    match data:
+        case 'All':
+            fld_in.data[:] = 1e10
+        case 'None':
+            fld_in.data[:] = 0
+        case 'Ocean':
+            fld_in.data[:] = mesh_out['mom_super_grid'].mesh.elementMask*1e20
+        case 'Land':
+            fld_in.data[:] = 1e-20*(mesh_out['mom_super_grid'].mesh.elementMask==0).astype(int)
 
     # for unclear reasons, we need to zero the output field before populating it
     # it looks like cells which are not destinations in remapping can introduce rounding error
-
     fld_out.data[:] = 0
 
     esmpy.RegridFromFile(fld_in, fld_out, filename=weights_file)
